7f089bc04cf4560bb0ae4e2fe4a5cba9294cb36f
Rikard Bartholf <rikard@bartholf.nu>
2022-12-04 19:20:19 +0100

    Cleanup

--- src/Controllers/Day3Controller.php
+++ src/Controllers/Day3Controller.php
@@ -10,20 +10,6 @@ use Slim\Psr7\{
 
 final class Day3Controller extends ControllerBase
 {
-    private array $priorities = [];
-
-    public function __construct()
-    {
-        $this->init();
-    }
-
-    private function init()
-    {
-        $this->priorities =
-            array_combine(range('a', 'z'), range(1, 26))
-            + array_combine(range('A', 'Z'), range(27, 52));
-    }
-
     public function index(Request $request, Response $response, array $args)
     {
         $result = Day3Model::dispatch();
