a5b66ee13f03d4b67cd786be65e33a9dfe4d8e66
Rikard Bartholf <rikard@bartholf.nu>
2022-12-04 15:35:56 +0100

    Cleanup

--- src/Controllers/Day4Controller.php
+++ src/Controllers/Day4Controller.php
@@ -9,6 +9,15 @@ use Slim\Psr7\{
 
 final class Day4Controller extends ControllerBase
 {
+    private static function getRange(string $ranges)
+    {
+        preg_match('/^(\d+)-(\d+),(\d+)-(\d+)$/', $ranges, $match);
+        return [
+            range($match[1], $match[2]),
+            range($match[3], $match[4]),
+        ];
+    }
+
     private static function isContained(array $a1, array $a2): bool
     {
         return array_intersect($a1, $a2) == $a1
@@ -21,11 +30,7 @@ final class Day4Controller extends ControllerBase
 
         $sum = 0;
         foreach ($rows as $x) {
-            preg_match('/^(\d+)-(\d+),(\d+)-(\d+)$/', $x, $match);
-
-            $sum += self::isContained(range($match[1], $match[2]), range($match[3], $match[4]))
-                ? 1
-                : 0;
+            $sum += self::isContained(...self::getRange($x)) ? 1 : 0;
         }
 
         echo 'Part1: ' . $sum . PHP_EOL;
@@ -37,14 +42,9 @@ final class Day4Controller extends ControllerBase
     public function part2()
     {
         $rows = array_filter(explode(PHP_EOL, $this->getFile('4-2')));
-
         $sum = 0;
         foreach ($rows as $x) {
-            preg_match('/^(\d+)-(\d+),(\d+)-(\d+)$/', $x, $match);
-
-            $sum += array_intersect(range($match[1], $match[2]), range($match[3], $match[4]))
-                ? 1
-                : 0;
+            $sum += array_intersect(...self::getRange($x)) ? 1 : 0;
         }
 
         echo 'Part2: ' . $sum . PHP_EOL;
