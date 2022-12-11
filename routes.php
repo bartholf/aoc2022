32aa90ff361b00b395655faf85d1d5a2ccf517f9
Rikard Bartholf <rikard@bartholf.nu>
2022-12-12 00:50:09 +0100

    Default to current year when routing

--- routes.php
+++ routes.php
@@ -8,5 +8,10 @@ use Slim\Psr7\{
 use Slim\App;
 
 return function (App $app) {
+    $app->get('/{id:[1-9][0-9]*}', function (Request $req, Response $res, $args) {
+        $url = sprintf('/%d/%d', date('Y'), $args['id']);
+        return $res->withHeader('Location', $url)->withStatus(301);
+    });
+
     $app->get('/{year}/{id:[1-9][0-9]*}', DayController::class . ':index');
 };
