acd27ca3595418ef28605952409b2d88ba05db90
Rikard Bartholf <rikard@bartholf.nu>
2022-12-13 14:27:49 +0100

    Initial commit day 13

---
+++ src/Models/y2022/Day13Model.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace AdventOfCode\Models\y2022;
+
+use AdventOfCode\Util\Indatafile;
+
+final class Day13Model extends ModelBase
+{
+    private Indatafile $indata;
+    private array $stack = [];
+
+    public function __construct()
+    {
+        $this->indata = Indatafile::load(__DIR__ . "/../../../data/{$this->getYear()}/13.txt");
+        $a = $this->indata->getArray();
+
+        $curr = [];
+        $i = 1;
+        foreach ($a as $x) {
+            if ($i++ % 3 === 0) {
+                $this->stack[] = $curr;
+                $curr = [];
+                continue;
+            }
+            $curr[] = json_decode($x);
+        }
+        $this->stack[] = $curr;
+        print_r($this->stack);
+    }
+
+    public function part1()
+    {
+        return 0;
+    }
+
+    public function part2()
+    {
+        return 0;
+    }
+}
